0d5f8e758dbf13fe2a637ff5fad2479520a553ce
Vitor Soares Vian <isvian@univates.br>
2019-05-17 18:50:20 -0300

    Adicionando comentario

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
+  // By default all proprietates of a component are restricted access
+  // 
   @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
