d2b0fba2f1d06a7be2f6b4ed354148098fda0500
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-09 17:14:25 -0300

    Data service

---
+++ services-start/src/app/account/account.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {LoggingService} from "../logging.service";
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
+})
+
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+  constructor(private logginService: LoggingService) {}
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.logginService.logStatusChange(status);
+  }
+}
