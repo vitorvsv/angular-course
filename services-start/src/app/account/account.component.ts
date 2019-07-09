8b48a96dcebf8f96d7c327f106fd9fd1505df318
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-09 20:49:50 -0300

    Inserting service in service using Injectable declaration

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,22 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountsService} from "../accounts.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private logginService: LoggingService) {}
+  constructor(private logginService: LoggingService, private accountService: AccountsService) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    this.logginService.logStatusChange(status);
+    this.accountService.updateStatus(this.id, status);
+    // this.logginService.logStatusChange(status);
   }
 }
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,22 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountsService} from "../accounts.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
-export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private logginService: LoggingService) {}
+export class NewAccountComponent {
+
+  constructor(private logginService: LoggingService, private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    this.logginService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
   }
 }
