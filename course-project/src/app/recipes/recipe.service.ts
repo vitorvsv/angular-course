dd4b972bf4b5b444b9770f1f2e712e6ee04b4bda
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-14 00:01:27 -0300

    Adding ingredients to recipes

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,13 +1,31 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter();
 
   private recipes: Recipe[] = [
-    new Recipe('One Recipe', 'This is the one pizza', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg'),
-    new Recipe('Two Recipe', 'This is the second pizza', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg')
+    new Recipe(
+      'Pizza',
+      'This is the one pizza',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg',
+      [
+        new Ingredient('Pasta', 1),
+        new Ingredient('Cheese', 1),
+        new Ingredient('Chicken', 1),
+      ]
+    ),
+    new Recipe(
+      'Hamburguer',
+      'This is the second pizza',
+      'https://i2.wp.com/hamburguergourmetcurso.com/wp-content/uploads/2018/11/curso_de_hamburguer_gourmet_completo_ima.png?resize=439%2C439&ssl=1',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 1),
+      ]
+    )
   ];
 
   getRecipes() {
