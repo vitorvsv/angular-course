672663f751db723edcf928402d4a63ab14c66532
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-15 21:21:27 -0300

    Sending and retriving parameters by URL dynamic

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
+
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+      private serversService: ServersService,
+      private router: Router,
+      private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
+  }
+
+  onReload() {
+    // this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
