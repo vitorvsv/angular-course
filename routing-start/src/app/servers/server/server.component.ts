64cc4a0440a81d841f3d13fd4ab5486065d767ea
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-18 21:21:43 -0300

    Add link to show server component

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Router} from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -19,7 +19,15 @@ export class ServerComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+
+    this.server = this.serversService.getServer(id);
+
+    this.route.params.subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+    );
   }
 
   onReload() {
