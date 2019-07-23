a921743c298a7da3a83f3b5f9dcb09fbc6344592
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-23 20:35:27 -0300

    Wildcard and redirectTo in routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,17 +11,21 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent },
-  ]},
+      { path: ':id/:name', component: UserComponent },
+    ]
+  },
   { path: 'servers', component: ServersComponent, children: [
-    { path: ':id/edit', component: EditServerComponent },
-    { path: ':id', component: ServerComponent },
-  ]},
-
+      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id', component: ServerComponent },
+    ]
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
@@ -32,7 +36,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
