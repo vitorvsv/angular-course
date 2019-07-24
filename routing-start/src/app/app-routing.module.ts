109065ce0d032317bbef13e0feb777cd9c551562
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-23 21:57:13 -0300

    Using canActivate to validate access to URL

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ServersComponent } from "./servers/servers.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -15,7 +16,7 @@ const appRoutes: Routes = [
             { path: ':id/:name', component: UserComponent },
         ]
     },
-    { path: 'servers', component: ServersComponent, children: [
+    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
             { path: ':id/edit', component: EditServerComponent },
             { path: ':id', component: ServerComponent },
         ]
@@ -26,7 +27,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports : [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes),
     ],
     exports : [
         RouterModule
