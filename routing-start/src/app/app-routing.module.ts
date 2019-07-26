3ad8f2ca0e36c35caca671a332a4f50ca395471f
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-26 17:38:50 -0300

    Added CanActivateChild to routes

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -16,7 +16,10 @@ const appRoutes: Routes = [
             { path: ':id/:name', component: UserComponent },
         ]
     },
-    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
+    { path: 'servers',
+        // canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: ServersComponent, children: [
             { path: ':id/edit', component: EditServerComponent },
             { path: ':id', component: ServerComponent },
         ]
--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import {
     ActivatedRouteSnapshot,
     CanActivate,
+    CanActivateChild,
     Router,
     RouterStateSnapshot
 } from "@angular/router";
@@ -10,12 +11,26 @@ import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
     constructor(private authService: AuthService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.authService.isAuthenticated().then(
+            (authenticated: boolean) => {
+                if (authenticated) {
+                    return true;
+                } else {
+                    this.router.navigate(['/']);
+                    return false;
+                }
+            }
+        );
+    }
+
+    canActivateChild(route: ActivatedRouteSnapshot,
+                     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         return this.authService.isAuthenticated().then(
             (authenticated: boolean) => {
                 if (authenticated) {
