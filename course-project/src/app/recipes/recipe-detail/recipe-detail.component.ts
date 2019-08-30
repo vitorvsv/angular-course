0ac886339ef7b3007052c26d2a35d15ccb4e00ca
Vitor Soares Vian <isvian@univates.br>
2019-08-30 19:32:31 -0300

    Select recipe active and pasta dinamic id by the url

--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
 
@@ -9,11 +11,15 @@ import {RecipeService} from "../recipe.service";
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+  recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.recipe = this.recipeService.getRecipe(+params['id']);
+    });
   }
 
   onAddToShoppingList() {
